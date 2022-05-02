9787b5e731f593ce39fe8609e2853103d18f9f0b
Maru32768 <maru32768@gmail.com>
2022-05-03 03:10:10 +0900

    setup

--- src/main/java/net/kunmc/lab/mobtowerdefence/MobTowerDefence.java
+++ src/main/java/net/kunmc/lab/mobtowerdefence/MobTowerDefence.java
@@ -1,8 +1,8 @@
-package net.kunmc.lab.paperplugintemplate;
+package net.kunmc.lab.mobtowerdefence;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PaperPluginTemplate extends JavaPlugin {
+public final class MobTowerDefence extends JavaPlugin {
     @Override
     public void onEnable() {
     }
